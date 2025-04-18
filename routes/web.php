ca1ec045296dfce601fb5335be93a028fc3b05af
Bao Truong <info@quocbao.dev>
2025-04-18 19:00:33 +0700

    Refactor route definitions for improved readability
    - Adjusted spacing for 'canLogin' and 'canRegister' parameters in the root route.
    - Standardized spacing for 'phpVersion' parameter.
    - Updated the require statement for auth.php to use consistent spacing.

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+        'canLogin'       => Route::has('login'),
+        'canRegister'    => Route::has('register'),
         'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+        'phpVersion'     => PHP_VERSION,
     ]);
 });
 
@@ -24,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
